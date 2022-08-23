b2539beb624419f466450c979712c33c720fb6dd
Gooood Mafia <goooodmafia@647231-ct04719.tmweb.ru>
2022-08-23 13:46:26 +0300

    upd

--- sh/get_price.sh
+++ sh/get_price.sh
@@ -4,5 +4,5 @@ rm ./in/good-opt.xls
 rm ./in/good-info.xls
 
 
-wget --directory-prefix=in http://old.gooood.ru/price/good-opt.xls
-wget --directory-prefix=in http://old.gooood.ru/price/good-info.xls
+wget --directory-prefix=in http://gooood.ru/price/good-opt.xls
+wget --directory-prefix=in http://gooood.ru/price/good-info.xls
